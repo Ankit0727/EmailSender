108e50bfdaedafc1c61717a4d92d988f2b2940c8
ANKIT CHOUDHARY <128247592+Ankit0727@users.noreply.github.com>
2024-11-15 21:38:20 +0530

    Add files via upload

---
+++ emailsenderdemo/src/main/java/com/emailsender/EmailsenderdemoApplication.java
@@ -0,0 +1,13 @@
+package com.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsenderdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailsenderdemoApplication.class, args);
+	}
+
+}
---
+++ emailsenderdemo/src/main/java/com/emailsender/service/EmailService.java
@@ -0,0 +1,15 @@
+package com.emailsender.service;
+
+import com.emailsender.entity.EmailDetails;
+
+//Interface
+public interface EmailService {
+
+	// Method
+	// To send a simple email
+	String sendSimpleMail(EmailDetails details);
+
+	// Method
+	// To send an email with attachment
+	String sendMailWithAttachment(EmailDetails details);
+}
